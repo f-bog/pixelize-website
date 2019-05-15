2d7570f6449f1d8414d349dd3e0f2c3ae4ae9273
Frederick <freddiebogdanoff@gmail.com>
2019-05-15 19:34:40 +1000

    removed layout file

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -5,37 +5,37 @@ import BlogNav from "../components/Navigation/BlogNav"
 import styled from "styled-components"
 import TextContainer from "../components/Containers/TextContainer"
 const PostContainer = styled.div``
-// export default class postLayout extends Component {
-//   render() {
-//     const { markdownRemark } = this.props.data
-//     return (
-//       <Layout>
-//         <PostContainer>
-//           <TextContainer>
-//             {" "}
-//             <div
-//               className="blog-post"
-//               dangerouslySetInnerHTML={{
-//                 __html: markdownRemark.html,
-//               }}
-//             />
-//             <BlogNav />
-//           </TextContainer>
-//         </PostContainer>
-//       </Layout>
-//     )
-//   }
-// }
+export default class postLayout extends Component {
+  render() {
+    const { markdownRemark } = this.props.data
+    return (
+      <Layout>
+        <PostContainer>
+          <TextContainer>
+            {" "}
+            <div
+              className="blog-post"
+              dangerouslySetInnerHTML={{
+                __html: markdownRemark.html,
+              }}
+            />
+            <BlogNav />
+          </TextContainer>
+        </PostContainer>
+      </Layout>
+    )
+  }
+}
 
-// export const query = graphql`
-//   query PostQuery($slug: String!) {
-//     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-//       html
-//       frontmatter {
-//         title
-//         date
-//         slug
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`
